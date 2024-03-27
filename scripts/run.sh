c864c2b8d20568004990787ebf447f9e884b22e3
igh197 <ingyuhan497@gmail.com>
2024-03-27 14:12:00 +0900

    final3

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-python manage.py wait_for_db
-python manage.py collectstatic --noinput
-python manage.py migrate
-
-# uWSGI: Nginx로부터 데이터를 받아오면 Django랑 소통을 하는 역활
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
